d6f092a524256781532e145f5e612767fa81b994
Marcia <marciaadler718@gmail.com>
2020-04-03 14:00:11 -0400

    updates

---
+++ models/Song.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+module.exports = function(sequelize, DataTypes) {
+  const Song = sequelize.define("Song", {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    }
+    title: Sequelize.STRING,
+    artist: Sequelize.STRING,
+    year: Sequelize.INTEGER,
+    duo: Sequelize.INTEGER,
+    explicit: Sequelize.INTEGER,
+    date_added: Sequelize.DATE,
+    styles: Sequelize.STRING,
+    languages: Sequelize.STRING
+  });
+  return Song;
+};
