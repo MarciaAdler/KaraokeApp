c460053dd1785bf6fbbfdbaf3b644ce60f3b18b4
Frank Moran <info@francisfrankmoran.com>
2020-04-21 15:15:40 -0400

    logged out top songs home

---
+++ client/src/components/HomeTopSongs.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Col, Row, Container } from "react-bootstrap";
+import topsongs from "../top-songs.json";
+
+function HomeTopSaved() {
+
+    console.log(topsongs);
+
+    function renderTopSongs(topsongs) {
+        return topsongs.map(song => {
+            return <Col sm={6}
+            md={3}
+            key={song.id}
+            className="my-3 px-4">
+                 <a href={song.link}>
+                <div
+                className="result-song--container px-3 py-3"
+                style={{ backgroundImage: `url(${song.image})` }}
+              >
+                    <div className="result-song--container__essentials text-center my-auto mx-auto h-100">
+                  <p className="result-song--title mb-2">{song.title}</p>
+                  <p className="result-song--artist">{song.artist}</p>
+
+                </div>
+
+                <div className="result-song--container__background">&nbsp;</div>
+                  </div>
+                  </a>
+                </Col>
+                
+        });
+        
+    }
+
+    return(
+        <Container fluid className="mt-5">
+            <Row>
+                <Col>
+                    <h4 className="home--saved-title">
+                        Top Karaoke Songs
+                    </h4>
+                </Col>
+            </Row>
+            <Row>
+                {renderTopSongs(topsongs)}
+            </Row>
+        </Container>
+    )
+}
+
+export default HomeTopSaved;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import Search from "../components/Search";
 import HomeSaved from "../components/HomeSaved";
+import HomeTopSongs from "../components/HomeTopSongs";
 import { useStoreContext } from "../utils/GlobalState";
 export default function Home() {
   const [state, dispatch] = useStoreContext();
@@ -11,6 +12,7 @@ export default function Home() {
     <div>
       <Hero />
       {!state.saved.length ? "" : <HomeSaved />}
+      <HomeTopSongs />
     </div>
   );
 }
