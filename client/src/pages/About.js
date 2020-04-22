506e90b1810400cc5601ab6aeb5071f5c7dc766b
Marcia <marciaadler718@gmail.com>
2020-04-22 14:01:23 -0400

    updated about page with karafun song list link

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,27 +2,57 @@ import React from "react";
 import { Col, Row, Container } from "react-bootstrap";
 
 export default function About() {
-
-
   return (
     <Container className="about py-3">
-        <Row>
-            <Col className="text-center">
-                <img className="logo" src={require("../img/logo.png")} />
-            </Col>
-        </Row>
-        <Row className="justify-content-center mt-3">
-            <Col className="about--container col-auto px-3 py-3">
-                <p>SingAlong Karaoke was developed by <a href="https://www.linkedin.com/in/francis-moran-63970665/" target="_blank">Francis Moran</a> and <a href="https://www.linkedin.com/in/marciaadler/" target="_blank">Marcia Adler</a>. </p>
+      <Row>
+        <Col className="text-center">
+          <img className="logo" src={require("../img/logo.png")} />
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-3">
+        <Col className="about--container col-auto px-3 py-3">
+          <p>
+            SingAlong Karaoke was developed by{" "}
+            <a
+              href="https://www.linkedin.com/in/francis-moran-63970665/"
+              target="_blank"
+            >
+              Francis Moran
+            </a>{" "}
+            and{" "}
+            <a href="https://www.linkedin.com/in/marciaadler/" target="_blank">
+              Marcia Adler
+            </a>
+            .{" "}
+          </p>
 
-                <p>Technologies used: React, Express, MySQL, Sequelize, Javascript, Bootstrap, Sass</p>
+          <p>
+            Technologies used: React, Express, MySQL, Sequelize, Javascript,
+            Bootstrap, Sass
+          </p>
 
-                <p>APIs used: Youtube API, Genius API, Musixmatch API</p>
+          <p>APIs used: Youtube API, Genius API, Musixmatch API</p>
 
-                <p>Credits: Duet icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+          <p>
+            Credits: Duet icons made by{" "}
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              {" "}
+              www.flaticon.com
+            </a>
+          </p>
 
-            </Col>
-        </Row>
+          <p>
+            Database songs from{" "}
+            <a href="https://www.karafun.com/karaoke-song-list.html">
+              Karafun song list
+            </a>
+          </p>
+        </Col>
+      </Row>
     </Container>
   );
 }
